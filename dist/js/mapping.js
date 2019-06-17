a642d67753e65d648762889eacd02ce82802df63
AK <a.kollmar@koenigsteiner.de>
2019-06-17 17:46:36 +0200

    prüft und passt die Felder an

--- dist/js/mapping.js
+++ dist/js/mapping.js
@@ -21,36 +21,53 @@ const mapFields = {
 		let contentLeft = mapFields.getJsons.editor();
 		let contentRight = mapFields.getJsons.editor2();
 
-		console.log(contentLeft);
-		console.log(contentRight);
-
-		Object.values(contentRight).forEach((content) => {
-			let temp;
-			console.log(content);
-			
-			// (content !== null) ? () => {
-				let arrList = content.toString().match(/(it.)[^+]*/gi);			
-	
-				if (Array.isArray(arrList) && arrList.length > 1 && arrList.length) {
-					for (let i = 0; i < arrList.length; i++) {
-						alert('relaxx-Feld erkannt: ' + arrList[i]);						
-					}
-				}
-				else {
-					if (Array.isArray(arrList) && arrList.length) {
-						alert('relaxx-Feld erkannt: ' + arrList[0]);
-					}
-					else {
-						console.log("Keine relaxx-Felder hinterlegt.");
-					}
-				}
-			// } : console.log("Wert ist null");
-
-		});
-
-
+		mapFields.lookingForRelaxxFields(contentRight);
 	},
 	"getChanges": () => {
 		alert(this.editor2);
+	},
+	"lookingForRelaxxFields": (jsonValues) => {
+		let boolFieldMapping = false;
+		Object.entries(jsonValues).forEach(([key, content]) => {
+			let temp;
+			
+			if (content !== null && typeof content !== 'object' && !Array.isArray(content)) {
+				let arrList = content.toString().match(/(it.)[^+]*/gi);
+				
+				if (Array.isArray(arrList) && arrList.length) {
+					jsonValues[key] = mapFields.mapNewValue(arrList);
+
+					boolFieldMapping = true;
+				}
+			}			
+		});
+		(boolFieldMapping === true) ? editor2.update(jsonValues) : alert("Keine Felder gemappt!");
+		
+	},
+	"mapNewValue": (searchingField) => {
+		
+		let contentLeft	= mapFields.getJsons.editor();
+		let newContent	= "";	
+		let searchField	= searchingField.map(value => value.substring(3, value.length).trim());
+		
+		searchField.forEach((field) => {
+			Object.entries(contentLeft).forEach(([key, value]) => {
+				if(mapFields.goThroughObject(contentLeft[key], key) === field){
+					newContent += value;
+				}
+			});
+		});
+		return newContent;
+	},
+	"goThroughObject": (objContent, objName) => {
+		if(typeof objContent !== 'object') {
+			return objName;
+		}
+		if(typeof objContent === 'object' && !Array.isArray(objContent)) {
+			Object.entries(objContent).forEach(([key, _value]) => {
+				console.log(key);
+				mapFields.goThroughObject(objContent[key], key);
+			})
+		}
 	}
 }
