5e029b80416c1b80b2eef7743223391096e9c634
AK <a.kollmar@koenigsteiner.de>
2019-06-18 15:39:16 +0200

    sucht nun durch Objekte hinweg. Jedoch keine array etc.

--- dist/js/mapping.js
+++ dist/js/mapping.js
@@ -28,46 +28,46 @@ const mapFields = {
 	},
 	"lookingForRelaxxFields": (jsonValues) => {
 		let boolFieldMapping = false;
+		let arrList;
 		Object.entries(jsonValues).forEach(([key, content]) => {
-			let temp;
-			
-			if (content !== null && typeof content !== 'object' && !Array.isArray(content)) {
-				let arrList = content.toString().match(/(it.)[^+]*/gi);
-				
-				if (Array.isArray(arrList) && arrList.length) {
-					jsonValues[key] = mapFields.mapNewValue(arrList);
 
-					boolFieldMapping = true;
-				}
-			}			
+			if (content !== null && typeof content !== 'object' && !Array.isArray(content)) {
+				let checkContent = content.toString().match(/(it.)[^+]*/gi);
+				if(checkContent !== null) {
+					arrList = checkContent;
+					if (Array.isArray(arrList) && arrList.length) {
+						jsonValues[key] = mapFields.mapNewValue(arrList);
+	
+						boolFieldMapping = true;
+					}
+				}				
+			}
 		});
-		(boolFieldMapping === true) ? editor2.update(jsonValues) : alert("Keine Felder gemappt!");
-		
+		(boolFieldMapping === true) ? editor2.update(jsonValues): alert("Keine Felder gemappt!");
+
 	},
 	"mapNewValue": (searchingField) => {
-		
-		let contentLeft	= mapFields.getJsons.editor();
-		let newContent	= "";	
-		let searchField	= searchingField.map(value => value.substring(3, value.length).trim());
-		
-		searchField.forEach((field) => {
-			Object.entries(contentLeft).forEach(([key, value]) => {
-				if(mapFields.goThroughObject(contentLeft[key], key) === field){
-					newContent += value;
+		let contentLeft = mapFields.getJsons.editor();
+		let newContent = "";
+		let searchField = searchingField.map(value => value.substring(3, value.length).trim());
+		searchField = searchField[0].split(".");
+		console.log(searchField);
+		for (let i = 0; i < searchField.length; i++) {
+			if (newContent === "") {
+				newContent = contentLeft[searchField[i]];
+				if(searchField.length === 1){
+					return newContent;
 				}
-			});
-		});
-		return newContent;
+			} else {
+				if (!(i === searchField.length - 1)) {
+					newContent = newContent[searchField[i]];
+				} else {
+					return newContent = newContent[searchField[i]];
+				}
+			}
+		}
 	},
 	"goThroughObject": (objContent, objName) => {
-		if(typeof objContent !== 'object') {
-			return objName;
-		}
-		if(typeof objContent === 'object' && !Array.isArray(objContent)) {
-			Object.entries(objContent).forEach(([key, _value]) => {
-				console.log(key);
-				mapFields.goThroughObject(objContent[key], key);
-			})
-		}
+
 	}
 }
