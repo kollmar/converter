91e39350e40aabaa3d6a036e2db3cf8562d88086
AK <a.kollmar@koenigsteiner.de>
2019-06-19 12:45:29 +0200

    Felder-Typabfrage ausgelagert

--- dist/js/mapping.js
+++ dist/js/mapping.js
@@ -17,6 +17,14 @@ const mapFields = {
 			return editor2.set(jsonObj);
 		}
 	},
+	"updateJsons": {
+		"editor": (jsonObj) => {
+			return editor.update(jsonObj);
+		},
+		"editor2": (jsonObj) => {
+			return editor2.update(jsonObj);
+		}
+	},
 	"changeContent": () => {
 		let contentLeft = mapFields.getJsons.editor();
 		let contentRight = mapFields.getJsons.editor2();
@@ -28,11 +36,54 @@ const mapFields = {
 	},
 	"lookingForRelaxxFields": (jsonValues) => {
 		let boolFieldMapping = false;
-		let arrList;
+		
 		Object.entries(jsonValues).forEach(([key, content]) => {
 
-			if (content !== null && typeof content !== 'object' && !Array.isArray(content)) {
-				let checkContent = content.toString().match(/(it.)[^+]*/gi);
+			// if (content !== null && typeof content !== 'object' && !Array.isArray(content)) {
+			// 	let checkContent = content.toString().match(/(it.)[^+]*/gi);
+			// 	if (checkContent !== null) {
+			// 		arrList = checkContent;
+			// 		if (Array.isArray(arrList) && arrList.length) {
+			// 			let chainContent = "";
+			// 			for (let i = 0; i < arrList.length; i++) {
+			// 				chainContent += mapFields.mapNewValue.json(arrList[i]);
+			// 			}
+			// 			jsonValues[key] = chainContent;
+
+			// 			boolFieldMapping = true;
+			// 		}
+			// 	}
+			// }
+			// else{
+			// 	if(Array.isArray(content)){
+			// 		let temp ="";
+			// 		for (let j = 0; j < content.length; j++) {
+			// 			let checkContent = content[j].toString().match(/(it.)[^+]*/gi);
+			// 			if (checkContent !== null) {
+			// 				arrList = checkContent;
+			// 				if (Array.isArray(arrList) && arrList.length) {
+			// 					let chainContent = "";
+			// 					for (let i = 0; i < arrList.length; i++) {
+			// 						chainContent += mapFields.mapNewValue.json(arrList[i]);
+			// 					}
+			// 					jsonValues[key][j] = chainContent;
+
+			// 					boolFieldMapping = true;
+			// 				}
+			// 			}
+			// 		}
+			// 	}
+			// }
+			mapFields.checkTypeOfFields.normalFields(content,jsonValues, key) ||
+			mapFields.checkTypeOfFields.array(content,jsonValues, key) ? boolFieldMapping = true : 0;
+		});
+		(boolFieldMapping === true) ? mapFields.updateJsons.editor2(jsonValues): alert("Keine Felder gemappt!");
+	},
+	"checkTypeOfFields": {
+		"normalFields" : (field, jsonValues, key) => {
+			let arrList;
+			if (field !== null && typeof field !== 'object' && !Array.isArray(field)) {
+				let checkContent = field.toString().match(/(it.)[^+]*/gi);
 				if (checkContent !== null) {
 					arrList = checkContent;
 					if (Array.isArray(arrList) && arrList.length) {
@@ -42,13 +93,37 @@ const mapFields = {
 						}
 						jsonValues[key] = chainContent;
 
-						boolFieldMapping = true;
+						return true;
 					}
 				}
 			}
-		});
-		(boolFieldMapping === true) ? editor2.update(jsonValues): alert("Keine Felder gemappt!");
+		},
+		"object" : (field) => {
 
+		},
+		"array" : (field, jsonValues, key) => {
+			console.log(field);
+			let arrList;
+			if(Array.isArray(field)){
+				let temp ="";
+				for (let j = 0; j < field.length; j++) {
+					console.log(field[j]);
+					let checkContent = field[j].toString().match(/(it.)[^+]*/gi);
+					if (checkContent !== null) {
+						arrList = checkContent;
+						if (Array.isArray(arrList) && arrList.length) {
+							let chainContent = "";
+							for (let i = 0; i < arrList.length; i++) {
+								chainContent += mapFields.mapNewValue.json(arrList[i]);
+							}
+							jsonValues[key][j] = chainContent;
+
+							return true;
+						}
+					}
+				}
+			}
+		}
 	},
 	"mapNewValue": {
 		"json": (searchingField) => {
