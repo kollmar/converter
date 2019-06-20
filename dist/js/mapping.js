a9d950c7f226d348dc476317bb51043d9d0c263f
AK <a.kollmar@koenigsteiner.de>
2019-06-20 17:01:56 +0200

    lässt sich nun durch Objekte und Arrays prüfen

--- dist/js/mapping.js
+++ dist/js/mapping.js
@@ -38,44 +38,9 @@ const mapFields = {
 		let boolFieldMapping = false;
 		
 		Object.entries(jsonValues).forEach(([key, content]) => {
-
-			// if (content !== null && typeof content !== 'object' && !Array.isArray(content)) {
-			// 	let checkContent = content.toString().match(/(it.)[^+]*/gi);
-			// 	if (checkContent !== null) {
-			// 		arrList = checkContent;
-			// 		if (Array.isArray(arrList) && arrList.length) {
-			// 			let chainContent = "";
-			// 			for (let i = 0; i < arrList.length; i++) {
-			// 				chainContent += mapFields.mapNewValue.json(arrList[i]);
-			// 			}
-			// 			jsonValues[key] = chainContent;
-
-			// 			boolFieldMapping = true;
-			// 		}
-			// 	}
-			// }
-			// else{
-			// 	if(Array.isArray(content)){
-			// 		let temp ="";
-			// 		for (let j = 0; j < content.length; j++) {
-			// 			let checkContent = content[j].toString().match(/(it.)[^+]*/gi);
-			// 			if (checkContent !== null) {
-			// 				arrList = checkContent;
-			// 				if (Array.isArray(arrList) && arrList.length) {
-			// 					let chainContent = "";
-			// 					for (let i = 0; i < arrList.length; i++) {
-			// 						chainContent += mapFields.mapNewValue.json(arrList[i]);
-			// 					}
-			// 					jsonValues[key][j] = chainContent;
-
-			// 					boolFieldMapping = true;
-			// 				}
-			// 			}
-			// 		}
-			// 	}
-			// }
 			mapFields.checkTypeOfFields.normalFields(content,jsonValues, key) ||
-			mapFields.checkTypeOfFields.array(content,jsonValues, key) ? boolFieldMapping = true : 0;
+			mapFields.checkTypeOfFields.array(content,jsonValues, key) ||
+			mapFields.checkTypeOfFields.object(content,jsonValues, key) ? boolFieldMapping = true : 0;
 		});
 		(boolFieldMapping === true) ? mapFields.updateJsons.editor2(jsonValues): alert("Keine Felder gemappt!");
 	},
@@ -90,16 +55,36 @@ const mapFields = {
 						let chainContent = "";
 						for (let i = 0; i < arrList.length; i++) {
 							chainContent += mapFields.mapNewValue.json(arrList[i]);
+							chainContent !== "undefined" ? jsonValues[key] = chainContent : alert(arrList[i] + " ist kein Feld");
 						}
-						jsonValues[key] = chainContent;
+						chainContent !== "undefined" ? jsonValues[key] = chainContent : 0;
 
 						return true;
 					}
 				}
 			}
 		},
-		"object" : (field) => {
-
+		"object" : (field, jsonValues, key) => {
+			let boolCheck = false;
+			if (typeof field === 'object' && !Array.isArray(field)) {
+				let arrKeyList = [];
+				Object.entries(field).forEach(([k, value]) => {
+					if(typeof field[k] === "object" && !Array.isArray(field[k])){
+						arrKeyList.push([k, field[k]]);
+					} else {
+						if(Array.isArray(field[k])) {
+							mapFields.checkTypeOfFields.array(field[k], jsonValues[key], k) ? boolCheck = true : 0;
+						} else {
+							mapFields.checkTypeOfFields.normalFields(field[k], jsonValues[key], k);
+						}
+					}
+				});
+				for (let i = 0; i < arrKeyList.length; i++) {
+					jsonValues[key][arrKeyList[i][0]];
+					mapFields.checkTypeOfFields.object(jsonValues[key][arrKeyList[i][0]], jsonValues[key], arrKeyList[i][0]);
+				}
+				return boolCheck;
+			}
 		},
 		"array" : (field, jsonValues, key) => {
 			let checkContent;
@@ -109,18 +94,21 @@ const mapFields = {
 				for (let j = 0; j < field.length; j++) {
 					if(Array.isArray(field[j])){
 						mapFields.checkTypeOfFields.array(field[j],jsonValues[key],j);
-					}
-					else {
-						let checkContent = field[j].toString().match(/(it.)[^+]*/gi);
-						if (checkContent !== null) {
-							arrList = checkContent;
-							if (Array.isArray(arrList) && arrList.length) {
-								let chainContent = "";
-								for (let i = 0; i < arrList.length; i++) {
-									chainContent += mapFields.mapNewValue.json(arrList[i]);
+					} else {						
+						if (typeof field[j] === "object" && !Array.isArray(field[j])){
+							mapFields.checkTypeOfFields.object(field[j],jsonValues[key],j);
+						} else {
+							let checkContent = field[j].toString().match(/(it.)[^+]*/gi);
+							if (checkContent !== null) {
+								arrList = checkContent;
+								if (Array.isArray(arrList) && arrList.length) {
+									let chainContent = "";
+									for (let i = 0; i < arrList.length; i++) {
+										chainContent += mapFields.mapNewValue.json(arrList[i]);
+										chainContent !== "undefined" ? jsonValues[key][j] = chainContent : alert(arrList[i] + " ist kein Feld");
+									}
+		
 								}
-								jsonValues[key][j] = chainContent;
-	
 							}
 						}
 					}
@@ -137,13 +125,27 @@ const mapFields = {
 			searchField = searchField.split(".");
 			for (let i = 0; i < searchField.length; i++) {
 				if (newContent === "") {
+					
 					newContent = contentLeft[searchField[i]];
+
 					if (searchField.length === 1) {
 						return newContent;
 					}
 				} else {
 					if (!(i === searchField.length - 1)) {
 						newContent = newContent[searchField[i]];
+						console.log(searchField[i]);
+						console.log(newContent);
+						if (Array.isArray(newContent)) {
+							for (let j = 0; j < newContent.length; j++) {
+								if (newContent[j].hasOwnProperty(searchField[i+1])){
+									newContent = newContent[j];
+								}
+								else{
+									console.log("Array " + j + " von " + searchField[i] + " - Feld (" + searchField[i+1] + ") nicht gefunden");
+								}
+							}
+						}
 					} else {
 						return newContent = newContent[searchField[i]];
 					}
