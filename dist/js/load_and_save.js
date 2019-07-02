b7d0e07eeb9cf59ac1b73ed7c6ae19460c1a4ba3
AK <a.kollmar@koenigsteiner.de>
2019-07-02 09:27:23 +0200

    speichern aller Informationen in einer JSON

--- dist/js/load_and_save.js
+++ dist/js/load_and_save.js
@@ -1,28 +1,52 @@
 // Load a JSON document
 FileReaderJS.setupInput(document.getElementById('loadDocument'), {
-    readAsDefault: 'Text',
-    on: {
-      load: function (event, file) {
-        editor2.setText(event.target.result);
-      }
-    }
-  });
+	readAsDefault: 'Text',
+	on: {
+		load: function (event, file) {
+			editor2.setText(event.target.result);
+		}
+	}
+});
 
-  // Save a JSON document
-  document.getElementById('saveDocument').onclick = function () {
-    // Save Dialog
-    fname = window.prompt("Save as...");
-    
-    // Check json extension in file name
-    if(fname.indexOf(".")==-1){
-      fname = fname + ".json";
-    }else{
-      if(fname.split('.').pop().toLowerCase() == "json"){
-        // Nothing to do
-      }else{
-        fname = fname.split('.')[0] + ".json";
-      }
-    } 
-    var blob = new Blob([editor2.getText()], {type: 'application/json;charset=utf-8'});
-    saveAs(blob, fname);
-  };
+// Save a JSON document
+document.getElementById('saveDocument').onclick = function () {
+	// Save Dialog
+	fname = window.prompt("Save as...");
+
+	// Check json extension in file name
+	if (fname.indexOf(".") == -1) {
+		fname = fname + ".json";
+	} else {
+		if (fname.split('.').pop().toLowerCase() == "json") {
+			// Nothing to do
+		} else {
+			fname = fname.split('.')[0] + ".json";
+		}
+	}
+	let project = {
+		"url": () => {
+			return document.getElementById('jsonURL').value
+		},
+		"urlJobs": () => {
+			return document.getElementById('jobFields').value
+		},
+		"dummyJobs": () => {
+			return document.getElementById('dummyFields').value
+		},
+		"dummy": () => {
+			return editor2.getText()
+		}
+	};
+	let jsonProject = '{"url":"' + project.url() + '"' +
+		',"urlJobs":"' + project.urlJobs() + '"' +
+		',"dummyJobs":"' + project.dummyJobs() + '"' +
+		',"dummy":' + project.dummy() + 
+		'}';
+
+	console.log(jsonProject);
+
+	var blob = new Blob([jsonProject], {
+		type: 'application/json;charset=utf-8'
+	});
+	saveAs(blob, fname);
+};
