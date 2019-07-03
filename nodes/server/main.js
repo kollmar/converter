6f7ef57c0bbda8a6002c3edf7a65399bdfeb2265
AK <a.kollmar@koenigsteiner.de>
2019-07-03 17:54:47 +0200

    Anpassungen und Tests sind noch drin

--- nodes/server/main.js
+++ nodes/server/main.js
@@ -1,29 +1,38 @@
-var fs          = require('fs');
-var Mapping     = require('../../dist/js/mapping');
-var request     = require('request');
+var fs = require('fs');
+var Mapping = require('../../dist/js/mapping');
+var request = require('request');
 var readContent = fs.readFileSync('test.json');
 var jsonContent = JSON.parse(readContent);
-var settings	= Mapping.mapFields.settings;
-console.log(settings.typeOfOutput);
-console.log(settings.show);
-
+var settings = Mapping.mapFields.settings;
 
 request({
 	url: jsonContent.url,
 	json: true,
+	'auth': {
+		'user': 'stepstone',
+		'pass': 'B@4u3HRsf$jU',
+		'sendImmediately': false
+	}
 }, (err, res, body) => {
 	try {
+		console.log(res.statusCode + ": " + res.statusMessage);
 		if (!err && res.statusCode === 200) {
 			settings.typeOfOutput = 'node';
-			settings.show         = 'output';
-			settings.urlJobs      = jsonContent.urlJobs;
-			settings.dummyField   = jsonContent.dummyJobs;
-			settings.dummy        = jsonContent.dummy;
-			settings.contentLeft  = body;
-			
-			console.log(Mapping.mapFields.addJobsToDummy(Mapping.mapFields.getCountedJobs, jsonContent.dummy));
-			// settings.dummy = Mapping.mapFields.addJobsToDummy(Mapping.mapFields.getCountedJobs, jsonContent.dummy);
-			// console.log(Mapping.mapFields.lookingForRelaxxFields(settings.dummy, settings.show));
+			settings.show = 'output';
+			settings.urlJobs = jsonContent.urlJobs;
+			settings.dummyField = jsonContent.dummyJobs;
+			settings.dummy = jsonContent.dummy;
+			settings.contentLeft = body;
+			settings.dummy = JSON.parse(JSON.stringify(Mapping.mapFields.addJobsToDummy(Mapping.mapFields.getCountedJobs(), settings.dummy)));
+			console.log(Mapping.mapFields.getCountedJobs());
+			fs.writeFile("output.json", JSON.stringify(Mapping.mapFields.lookingForRelaxxFields(settings.dummy, settings.show)), function (err) {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log("The file was saved!");
+				}
+			});
+			// Mapping.mapFields.lookingForRelaxxFields(settings.dummy, settings.show);
 		}
 	} catch (err) {
 		console.log(err);
