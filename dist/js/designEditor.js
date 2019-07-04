acf8bb15f8d5e775956e7d96ac0a177fa585f693
pkollmar <kollmarp@gmail.com>
2019-07-04 08:28:58 +0200

    script für 'Durchsuchen' in designEditor.js ausgelagert

--- dist/js/designEditor.js
+++ dist/js/designEditor.js
@@ -1,33 +1,50 @@
- var container = document.getElementById('jsoneditor2');
+const uploadButton = document.querySelector('.browse-btn');
+const fileInfo = document.querySelector('.file-info');
+const realInput = document.getElementById('real-input');
 
- var json = {
-    'array': [1, 2, 3],
-    'boolean': true,
-    'null': null,
-    'number': 123,
-    'object': {'a': 'b', 'c': 'd'},
-    'string': 'Hello World'
-  };
+uploadButton.addEventListener('click', (e) => {
+  realInput.click();
+});
 
-  var editor = new JSONEditor(container, options, json);
+realInput.addEventListener('change', () => {
+  const name = realInput.value.split(/\\|\//).pop();
+  const truncated = name.length > 20 
+    ? name.substr(name.length - 20) 
+    : name;
+  
+  fileInfo.innerHTML = truncated;
+});
 
-const list = [
-    "cursor-pointer btn-sm fa fa-folder-open",
-    "bacursor-pointer btn-sm fa fa-floppy-or",
-    "cursor-pointer btn-sm fa fa-check",
-    "cursor-pointer btn-sm fa fa-print",
-    "cursor-pointer btn-sm fa fa-times",
-    "cursor-pointer btn-sm btn-shrink fa fa-files-o",
-    "cursor-pointer btn-sm btn-fullscreen fa fa-arrows-alt",
-]
-var btnList2 ="";
-for (let i = 0; i < list.length; i++) {
-    btnList2 += '<li class="' + list[i] + '" style="background:none; margin:4px 3px 0 3px; font-family:"Font Awesome 5 Pro";"><\/li>';
-}
+//  var container = document.getElementById('jsoneditor2');
+
+//  var json = {
+//     'array': [1, 2, 3],
+//     'boolean': true,
+//     'null': null,
+//     'number': 123,
+//     'object': {'a': 'b', 'c': 'd'},
+//     'string': 'Hello World'
+//   };
+
+//   var editor = new JSONEditor(container, options, json);
+
+// const list = [
+//     "cursor-pointer btn-sm fa fa-folder-open",
+//     "bacursor-pointer btn-sm fa fa-floppy-or",
+//     "cursor-pointer btn-sm fa fa-check",
+//     "cursor-pointer btn-sm fa fa-print",
+//     "cursor-pointer btn-sm fa fa-times",
+//     "cursor-pointer btn-sm btn-shrink fa fa-files-o",
+//     "cursor-pointer btn-sm btn-fullscreen fa fa-arrows-alt",
+// ]
+// var btnList2 ="";
+// for (let i = 0; i < list.length; i++) {
+//     btnList2 += '<li class="' + list[i] + '" style="background:none; margin:4px 3px 0 3px; font-family:"Font Awesome 5 Pro";"><\/li>';
+// }
 
 
 // document.getElementById('jsoneditor')
 //     .innerHTML += btnList;
-document.getElementsByClassName('jsoneditor-menu')[0]
-    .innerHTML += btnList2;
+// document.getElementsByClassName('jsoneditor-menu')[0]
+//     .innerHTML += btnList2;
 
--- dist/js/jsonEditor.js
+++ dist/js/jsonEditor.js
@@ -117,20 +117,3 @@ editor2.set(json);
 
 // get json
 // json = editor.get();
-
-const uploadButton = document.querySelector('.browse-btn');
-const fileInfo = document.querySelector('.file-info');
-const realInput = document.getElementById('real-input');
-
-uploadButton.addEventListener('click', (e) => {
-  realInput.click();
-});
-
-realInput.addEventListener('change', () => {
-  const name = realInput.value.split(/\\|\//).pop();
-  const truncated = name.length > 20 
-    ? name.substr(name.length - 20) 
-    : name;
-  
-  fileInfo.innerHTML = truncated;
-});
