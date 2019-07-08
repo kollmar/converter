e8593b56bf41eaf264866a113046873a8c7639d1
pkollmar <51698686+pkollmar@users.noreply.github.com>
2019-07-08 09:31:27 +0200

    Design (#9)
    * button änderung
    
    * icon undo/redo heller
    
    * #jsonURL, #jobFields - 'Problems (3) Fehler fixed' display: inline > display: inline-block
    
    * fixed jumping load button, fixed input:hover border > input[type=text]:hover border
    
    * fixed input type'url' - hover border hinzugefügt
    
    * add svg Logo
    
    * keine großen Änderungen
    
    * buttons farblich angepasst
    
    * Microadnimationen hinzugefügt, Farben der Buttons angepasst
    
    * weitere Buttons wurden angepasst
    
    * Buttons angepasst sowie animationen
    
    * script für 'Durchsuchen' in designEditor.js ausgelagert
    
    * Auslagerung der design.css und JSON Date upload Funktion gefixt
    
    * json upload - name wird vollständig angezeigt
    
    * Buttons: Mauszeiger änder sich, Schatten entfernt; Loading-text: ist jetzt scrollbar; Ränder der Editoren verkleienert
    
    * kein button:focus mehr
    
    * 'Problems' - .file-info / inline-block gelöscht, da useless
    
    * Authentication: Check Slider, Name, Password - Felder erstellt; Funktion noch nicht vorhanden
    
    * check button für auth hinzugefügt
    
    * angepasst Auth-Bereich

--- dist/js/designEditor.js
+++ dist/js/designEditor.js
@@ -1,33 +1,50 @@
- var container = document.getElementById('jsoneditor2');
+const uploadButton = document.querySelector('.browse-btn');
+const fileInfo = document.querySelector('.file-info');
+const realInput = document.querySelector('.real-input');
 
- var json = {
-    'array': [1, 2, 3],
-    'boolean': true,
-    'null': null,
-    'number': 123,
-    'object': {'a': 'b', 'c': 'd'},
-    'string': 'Hello World'
-  };
+uploadButton.addEventListener('click', (e) => {
+  realInput.click();
+});
 
-  var editor = new JSONEditor(container, options, json);
+realInput.addEventListener('change', () => {
+  const name = realInput.value.split(/\\|\//).pop();
+  fileInfo.innerHTML = name;
+});
 
-const list = [
-    "cursor-pointer btn-sm fa fa-folder-open",
-    "bacursor-pointer btn-sm fa fa-floppy-or",
-    "cursor-pointer btn-sm fa fa-check",
-    "cursor-pointer btn-sm fa fa-print",
-    "cursor-pointer btn-sm fa fa-times",
-    "cursor-pointer btn-sm btn-shrink fa fa-files-o",
-    "cursor-pointer btn-sm btn-fullscreen fa fa-arrows-alt",
-]
-var btnList2 ="";
-for (let i = 0; i < list.length; i++) {
-    btnList2 += '<li class="' + list[i] + '" style="background:none; margin:4px 3px 0 3px; font-family:"Font Awesome 5 Pro";"><\/li>';
-}
+function myFunction() {
+  document.getElementById("slider").disabled == true;
+});
+
+//  var container = document.getElementById('jsoneditor2');
+
+//  var json = {
+//     'array': [1, 2, 3],
+//     'boolean': true,
+//     'null': null,
+//     'number': 123,
+//     'object': {'a': 'b', 'c': 'd'},
+//     'string': 'Hello World'
+//   };
+
+//   var editor = new JSONEditor(container, options, json);
+
+// const list = [
+//     "cursor-pointer btn-sm fa fa-folder-open",
+//     "bacursor-pointer btn-sm fa fa-floppy-or",
+//     "cursor-pointer btn-sm fa fa-check",
+//     "cursor-pointer btn-sm fa fa-print",
+//     "cursor-pointer btn-sm fa fa-times",
+//     "cursor-pointer btn-sm btn-shrink fa fa-files-o",
+//     "cursor-pointer btn-sm btn-fullscreen fa fa-arrows-alt",
+// ]
+// var btnList2 ="";
+// for (let i = 0; i < list.length; i++) {
+//     btnList2 += '<li class="' + list[i] + '" style="background:none; margin:4px 3px 0 3px; font-family:"Font Awesome 5 Pro";"><\/li>';
+// }
 
 
 // document.getElementById('jsoneditor')
 //     .innerHTML += btnList;
-document.getElementsByClassName('jsoneditor-menu')[0]
-    .innerHTML += btnList2;
+// document.getElementsByClassName('jsoneditor-menu')[0]
+//     .innerHTML += btnList2;
 
